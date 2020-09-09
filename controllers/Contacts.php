83154000fbe619129fab25be1130599f0b812103
unknown <delvaux.robby@gmail.com>
2020-09-09 13:36:37 +0200

    OK ?

--- controllers/Contacts.php
+++ controllers/Contacts.php
@@ -3,25 +3,15 @@
         public function index(){
             echo 'Hello World !!!';
             $this->loadModel('Contact');
-<<<<<<< HEAD
             $contacts = $this->Contacts->getAll();
-=======
-            $contact = $this->Contact->getAll();
->>>>>>> Master
             
         }
         public function lire(string $slug){
             $this->loadModel('Contact');
 
-<<<<<<< HEAD
             $contacts = $this->Contacts->findBySlug($slug);
 
             $this->render('lire' , compact('contacts'));
-=======
-            $contact = $this->Contact->findBySlug($slug);
-
-            $this->render('lire' , compact('contact'));
->>>>>>> Master
         }
     }
     
